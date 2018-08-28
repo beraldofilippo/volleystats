b0cbbe2e3282cfd752673c767ca78b16f583df8a
Filippo Beraldo <beraldofilippo@gmail.com>
2018-08-28 16:48:49 +0100

    Added Nunito font, using it in the main theme

--- lib/style/theme.dart
+++ lib/style/theme.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:volleystats/style/colors.dart';
 
-final ThemeData themeData = new ThemeData(
-    primarySwatch: ThemeColors.getSwatch()
-);
+final ThemeData themeData =
+    new ThemeData(
+        primarySwatch: ThemeColors.getSwatch(),
+        fontFamily: 'Nunito');
 
 ThemeData getThemeData() {
   return themeData;
